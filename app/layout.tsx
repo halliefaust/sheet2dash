716098285b16685a400b514574267c697755ce0d
Hallie Faust <hfaust@usc.edu>
2025-02-14 23:28:36 -0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Sheet2Dash',
   description: 'Created with v0',
+  icons: {
+    icon: '/favicon.ico',
+  },
   generator: 'v0.dev',
 }
 
